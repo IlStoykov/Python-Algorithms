1a8fc891051f27303d3742b92f9b51cc5079d9e5
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-08-16 21:26:15 +0300

    simple recursion request

---
+++ Recursion/Lectures.py
@@ -0,0 +1,15 @@
+"""sum of list wint nums with recursion"""
+# def sum_array(nums, index):
+#     if index == len(nums)-1:
+#         return nums[index]
+#     return nums[index] + sum_array(nums, index + 1)
+#
+# nums = [int(x) for x in input().split()]
+# print(sum_array(nums, 0))
+"""find factorials with recursion"""
+# def calc_fact(n):
+#     if n == 1:
+#         return 1
+#     return n * calc_fact(n-1)
+# num = int(input())
+# print (calc_fact(num))
