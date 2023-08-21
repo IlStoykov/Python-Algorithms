a75943310476d3f604ea3af543207d766b24eb4f
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-08-21 21:49:56 +0300

    Update Lectures.py

--- Recursion/Lectures.py
+++ Recursion/Lectures.py
@@ -63,32 +63,68 @@
 # find_combinations(0, vector)
 
 """"finding the way out with backtracking"""
-def find_exit(row, col, lab, destination, path):
-    if row < 0 or col < 0 or row >= len(lab) or col >= len(lab[0]):
+# def find_exit(row, col, lab, destination, path):
+#     if row < 0 or col < 0 or row >= len(lab) or col >= len(lab[0]):
+#         return
+#     if lab[row][col] == "*":
+#         return
+#     if lab[row][col] == "v":
+#         return
+#     path.append(destination)
+#     if lab[row][col] == "e":
+#         print("".join(path))
+#
+#     else:
+#
+#         lab[row][col] = "v"
+#
+#         find_exit(row - 1, col, lab, "U", path)
+#         find_exit(row + 1, col, lab, "D", path)
+#         find_exit(row, col - 1, lab, "L", path)
+#         find_exit(row, col + 1, lab, "R", path)
+#         lab[row][col] = "-"
+#     path.pop()
+#
+# rows = int(input())
+# cols = int(input())
+# lab = []
+# for _ in range(rows):
+#     lab.append(list(input()))
+#
+# find_exit(0, 0, lab, "", [])
+
+""" 8 Queens"""
+
+
+def print_board(board):
+    for row in board:
+        print(" ".join(board))
+    print()
+
+def check_queen(row, col, rows, cols, left_diagonals, right_diagonals):
+    if row in rows:
+        return False
+    if col in cols:
+        return False
+    if (row - col) in left_diagonals:
+        return False
+    if (row + col) in right_diagonals:
+        return False
+    return True
+
+
+def set_queen(row, col, rows, cols, left_diagonals, right_diagonals):
+    pass
+
+
+def put_queen(row, board, rows, cols, left_diagonals, right_diagonals):
+    if row == 8:
+        print_board(board)
         return
-    if lab[row][col] == "*":
-        return
-    if lab[row][col] == "v":
-        return
-    path.append(destination)
-    if lab[row][col] == "e":
-        print("".join(path))
-
-    else:
-
-        lab[row][col] = "v"
-
-        find_exit(row - 1, col, lab, "U", path)
-        find_exit(row + 1, col, lab, "D", path)
-        find_exit(row, col - 1, lab, "L", path)
-        find_exit(row, col + 1, lab, "R", path)
-        lab[row][col] = "-"
-    path.pop()
-
-rows = int(input())
-cols = int(input())
-lab = []
-for _ in range(rows):
-    lab.append(list(input()))
-
-find_exit(0, 0, lab, "", [])
+    for col in range(8):
+        if check_queen(row, col, rows, cols, left_diagonals, right_diagonals):
+            set_queen(row, col, rows, cols, left_diagonals, right_diagonals)
+n = 8
+board = []
+[board.append("-" * 8) for _ in range(8)]
+put_queen(row, board, set(), set(), set(), set())
