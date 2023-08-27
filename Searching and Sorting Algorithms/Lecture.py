2fc368a5bcc95e1119867d2bc235a410f888cb3c
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-08-27 15:20:02 +0300

    Update Lecture.py

--- Searching and Sorting Algorithms/Lecture.py
+++ Searching and Sorting Algorithms/Lecture.py
@@ -24,7 +24,7 @@
 #
 # nums = [int(x) for x in input().split()]
 # target_num = int(input())
-
+#
 # print(binary_search(nums, target_num))
 
 """Binary Search"""
@@ -65,19 +65,58 @@
 #     return nums
 #
 # nums = [int(x) for x in input().split()]
-# print(selct_sort(nums))
+# print(*selct_sort(nums), sep= " ")
 
 """Bubble Sort"""
-def bubble_sort(nums):
-    counter = 0
-    is_sorted = False
-    while not is_sorted:
-        is_sorted = True
-        for j in range(1, len(nums) - counter):
-            if nums[j] < nums[j - 1]:
-                nums[j], nums[j - 1] = nums[j - 1], nums[j]
-                is_sorted = False
-        counter += 1
-    return nums
-nums = [int(x) for x in input().split()]
-print(*bubble_sort(nums), sep=" ")
+# def bubble_sort(nums):
+#     counter = 0
+#     is_sorted = False
+#     while not is_sorted:
+#         is_sorted = True
+#         for j in range(1, len(nums) - counter):
+#             if nums[j] < nums[j - 1]:
+#                 nums[j], nums[j - 1] = nums[j - 1], nums[j]
+#                 is_sorted = False
+#         counter += 1
+#     return nums
+# nums = [int(x) for x in input().split()]
+# print(*bubble_sort(nums), sep=" ")
+
+"""Insertion Sort"""
+# def insert_sort(nums):
+#     for i in range(len(nums)):
+#         for j in range(i, 0, -1):
+#             if nums[j] < nums[j -1]:
+#                 nums[j], nums[j -1] = nums[j -1], nums[j]
+#             else:
+#                 break
+#     return nums
+# nums = [int(x) for x in input().split()]
+# print(*insert_sort(nums), sep = " ")
+
+"""Quicksort"""
+"""selecting a pivot element and partitioning the array around the pivot, 
+such that all elements less than the pivot are moved to its left and all elements greater than the pivot are moved to its right. 
+Then, it recursively applies the same process to the left and right sub-arrays until the entire array is sorted."""
+# def quicksort_funk(start, end, nums):
+#     if start >= end:
+#         return
+#     pivot = start
+#     left = start + 1
+#     right = end
+#
+#     while left <= right:
+#         if nums[left] > nums[pivot] > nums[right]:
+#             nums[left], nums[right] = nums[right], nums[left]
+#         if nums[left] <= nums[pivot]:
+#             left += 1
+#         if nums[right] >= nums[pivot]:
+#             right -= 1
+#
+#     nums[right], nums[pivot] = nums[pivot], nums[right]
+#     quicksort_funk(pivot, right - 1, nums)
+#     quicksort_funk(left, end, nums)
+#
+# nums = [int(x) for x in input().split()]
+# quicksort_funk(0, len(nums) -1, nums)
+# print(*nums, sep = " ")
