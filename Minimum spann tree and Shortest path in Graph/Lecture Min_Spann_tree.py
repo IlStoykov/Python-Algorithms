605735b28906fadcbd065e95224bdddc286dec0c
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-09-07 22:11:11 +0300

    Update Lecture Min_Spann_tree.py

--- Minimum spann tree and Shortest path in Graph/Lecture Min_Spann_tree.py
+++ Minimum spann tree and Shortest path in Graph/Lecture Min_Spann_tree.py
@@ -1,1 +1,38 @@
+from collections import  deque
 """1.	Shortest Path in Unweighted Graph"""
+nodes = int(input())
+edges = int(input())
+
+graph = []
+[graph.append([]) for _ in range(nodes + 1)]
+
+for _ in range(edges):
+    source, destination = [int(x) for x in input().split()]
+    graph[source].append(destination)
+
+start_node = int(input())
+destination_node = int(input())
+
+visited = [False] * (nodes + 1)
+parent = [None] * (nodes + 1)
+
+visited[start_node] = True
+queue = deque([start_node])
+
+while queue:
+    node = queue.popleft()
+    if node == destination_node:
+        break
+    for child in graph[node]:
+        if visited[child]:
+            continue
+        visited[child] = True
+        queue.append(child)
+        parent[child] = node
+
+result = deque()
+node = destination_node
+while node is not None:
+    result.appendleft(node)
+    node = parent[node]
+print(*result)
