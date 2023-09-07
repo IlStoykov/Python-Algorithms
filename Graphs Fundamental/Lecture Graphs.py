efcf260049ed6641681bf2649b126bed3543b2f1
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-09-07 21:01:39 +0300

    Update Lecture Graphs.py

--- Graphs Fundamental/Lecture Graphs.py
+++ Graphs Fundamental/Lecture Graphs.py
@@ -106,3 +106,52 @@
 #     component = []
 #     dfs(node, visited, graph, component)
 #     print(f"Connected component: {' '.join([str(x) for x in component])}")
+
+"""Source Removal Topological Sorting"""
+# def find_dependencies(graph):
+#     result = {}
+#     for node, children in graph.items():
+#         if node not in result:
+#             result[node] = 0
+#         for child in children:
+#             if child not in result:
+#                 result[child] = 1
+#             else:
+#                 result[child] += 1
+#     return result
+#
+# """checking for 0 dendencies. if None cycle found"""
+# def find_node_without_dependencies(dependencies_by_node):
+#     for node, dependencies in dependencies_by_node.items():
+#         if dependencies == 0:
+#             return node
+#     return None
+#
+# graph = {}
+# nodes = int(input())
+# for _ in range(nodes):
+#     line = input().split("->")
+#     node = line[0].strip()
+#     children = line[1].strip().split(", ") if line[1] else []
+#     graph[node] = children
+#
+# """finding each nods how much dependencies has"""
+# dependencies_by_node = find_dependencies(graph)
+# has_cycle = False
+# result = []
+#
+# # finding node without dependencies
+# while dependencies_by_node:
+#     node_to_remove = find_node_without_dependencies(dependencies_by_node)
+#     if node_to_remove is None:
+#         has_cycle = True
+#         break
+#     dependencies_by_node.pop(node_to_remove)
+#     result.append(node_to_remove)
+#     for child in graph[node_to_remove]:
+#         dependencies_by_node[child] -= 1
+#
+# if has_cycle:
+#     print("Invalid topological sorting")
+# else:
+#     print(f"Topological sorting: {', '.join([str(x) for x in result])}")
