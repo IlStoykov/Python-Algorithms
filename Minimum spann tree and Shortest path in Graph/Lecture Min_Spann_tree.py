7353825d92ad3c62e4fc8a04ead465ecec5a236c
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-09-09 22:14:40 +0300

    Update Lecture Min_Spann_tree.py

--- Minimum spann tree and Shortest path in Graph/Lecture Min_Spann_tree.py
+++ Minimum spann tree and Shortest path in Graph/Lecture Min_Spann_tree.py
@@ -1,38 +1,92 @@
-from collections import  deque
 """1.	Shortest Path in Unweighted Graph"""
-nodes = int(input())
+# from collections import  deque
+# nodes = int(input())
+# edges = int(input())
+#
+# graph = []
+# [graph.append([]) for _ in range(nodes + 1)]
+#
+# for _ in range(edges):
+#     source, destination = [int(x) for x in input().split()]
+#     graph[source].append(destination)
+#
+# start_node = int(input())
+# destination_node = int(input())
+#
+# visited = [False] * (nodes + 1)
+# parent = [None] * (nodes + 1)
+#
+# visited[start_node] = True
+# queue = deque([start_node])
+#
+# while queue:
+#     node = queue.popleft()
+#     if node == destination_node:
+#         break
+#     for child in graph[node]:
+#         if visited[child]:
+#             continue
+#         visited[child] = True
+#         queue.append(child)
+#         parent[child] = node
+#
+# result = deque()
+# node = destination_node
+# while node is not None:
+#     result.appendleft(node)
+#     node = parent[node]
+# print(*result)
+
+"""2.	Dijkstra's Algorithm"""
+# Finding the shortest path between two nodes in an unweighted graph is done by applying simple BFS.
+from queue import PriorityQueue
+class Edge:
+    def __init__(self, source, destination, weight):
+        self.source = source
+        self.destination = destination
+        self.weight = weight
+
+
 edges = int(input())
-
-graph = []
-[graph.append([]) for _ in range(nodes + 1)]
-
+graph = {}
+# reading and constructing graph with class
 for _ in range(edges):
-    source, destination = [int(x) for x in input().split()]
-    graph[source].append(destination)
+    source, destination, weight = [int(x) for x in input().split(", ")]
+    if source not in graph:
+        graph[source] = []
+    if destination not in graph:
+        graph[destination] = []
+    graph[source].append(Edge(source, destination, weight))
 
-start_node = int(input())
-destination_node = int(input())
+start = int(input())
+target = int(input())
+# find graph length and define graph distance [] and parent []
+max_node = max(graph.keys())
 
-visited = [False] * (nodes + 1)
-parent = [None] * (nodes + 1)
+distance = [float('inf')] * max_node
+parent = [None] * max_node
 
-visited[start_node] = True
-queue = deque([start_node])
+# define priority queue and set start node there for bfs
+pq = PriorityQueue()
+pq.put((0, start))
 
-while queue:
-    node = queue.popleft()
-    if node == destination_node:
+while not pq.empty():
+    min_distance, node = pq.get()
+    if node == target:
         break
-    for child in graph[node]:
-        if visited[child]:
-            continue
-        visited[child] = True
-        queue.append(child)
-        parent[child] = node
+    for edge in graph[node]:
+        if edge == float('inf'):
+            # add the edge to pq with weight from prev nodes and it`s own weight
+            new_distance = min_distance + edge.weight
+            # should check if new distance is smaller than exit and if it is change to new one
+            if new_distance < distance[edge.destination]:
+                distance[edge.destination] = new_distance
+                parent[edge.destination] = node
+                pq.put(new_distance, edge.destination)
+print(distance[])
 
-result = deque()
-node = destination_node
-while node is not None:
-    result.appendleft(node)
-    node = parent[node]
-print(*result)
+
+
+
+
+
