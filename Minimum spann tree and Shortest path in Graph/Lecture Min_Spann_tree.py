2b6960b8b8957d29de4a5b8223576f04104821fe
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-09-11 08:54:03 +0300

    Update Lecture Min_Spann_tree.py

--- Minimum spann tree and Shortest path in Graph/Lecture Min_Spann_tree.py
+++ Minimum spann tree and Shortest path in Graph/Lecture Min_Spann_tree.py
@@ -39,52 +39,63 @@
 
 """2.	Dijkstra's Algorithm"""
 # Finding the shortest path between two nodes in an unweighted graph is done by applying simple BFS.
-# from queue import PriorityQueue
-# class Edge:
-#     def __init__(self, source, destination, weight):
-#         self.source = source
-#         self.destination = destination
-#         self.weight = weight
-#
-#
-# edges = int(input())
-# graph = {}
-# # reading and constructing graph with class
-# for _ in range(edges):
-#     source, destination, weight = [int(x) for x in input().split(", ")]
-#     if source not in graph:
-#         graph[source] = []
-#     if destination not in graph:
-#         graph[destination] = []
-#     graph[source].append(Edge(source, destination, weight))
-#
-# start = int(input())
-# target = int(input())
-# # find graph length and define graph distance [] and parent []
-# max_node = max(graph.keys())
-#
-# distance = [float('inf')] * max_node
-# parent = [None] * max_node
-#
-# distance[start] = 0
-# # define priority queue and set start node there for bfs
-# pq = PriorityQueue()
-# pq.put((0, start))
-#
-# while not pq.empty():
-#     min_distance, node = pq.get()
-#     if node == target:
-#         break
-#     for edge in graph[node]:
-#         # should check if new distance is smaller than exit and if it is change to new one
-#         new_distance = min_distance + edge.weight
-#         if new_distance < distance[edge.destination]:
-#             # add the edge to pq with weight from prev nodes and it`s own weight
-#             distance[edge.destination] = new_distance
-#             parent[edge.destination] = node
-#             pq.put(new_distance, edge.destination)
-#
-# print(distance[target])
+from queue import PriorityQueue
+from collections import  deque
+class Edge:
+    def __init__(self, source, destination, weight):
+        self.source = source
+        self.destination = destination
+        self.weight = weight
+
+
+edges = int(input())
+graph = {}
+# reading and constructing graph with class for edges
+for _ in range(edges):
+    source, destination, weight = [int(x) for x in input().split(", ")]
+    if source not in graph:
+        graph[source] = []
+    if destination not in graph:
+        graph[destination] = []
+    graph[source].append(Edge(source, destination, weight))
+
+start = int(input())
+target = int(input())
+# find graph length and define graph distance [] and parent []
+max_node = max(graph.keys())
+
+distance = [float('inf')] * (max_node + 1)
+parent = [None] * (max_node + 1)
+
+distance[start] = 0
+# define priority queue and set start node there for bfs
+pq = PriorityQueue()
+pq.put((0, start))
+
+while not pq.empty():
+    min_distance, node = pq.get()
+    if node == target:
+        break
+    for edge in graph[node]:
+        # should check if new distance is smaller than exit and if it is change to new one
+        new_distance = min_distance + edge.weight
+        if new_distance < distance[edge.destination]:
+            # add the edge to pq with weight from prev nodes and it`s own weight
+            distance[edge.destination] = new_distance
+            parent[edge.destination] = node
+            pq.put((new_distance, edge.destination))
+
+if distance[target] == float ("inf"):
+    print('There is no such path. ')
+else:
+    print(distance[target])
+
+    path = deque()
+    node = target
+    while node is not None:
+        path.appendleft(node)
+        node = parent[node]
+    print(*path, end=" ")
 
 
 
